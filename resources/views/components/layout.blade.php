56faebd4435edb079dcbb581c562fce2db9f7202
Soung Oo Lwin <soungoolwin275@gmail.com>
2025-05-20 19:18:12 +0700

    add logo and title at browser header bug fix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <link rel="icon" type="image/png" href="{{ Vite::asset('public/images/logo.png') }}">
+    <link rel="icon" type="image/png" href="/images/logo.png">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ 'Scentagory' }}</title>
