11ea0c9d8d21703ecfefd6720e67bdf04d729156
Soung Oo Lwin <soungoolwin275@gmail.com>
2025-05-25 17:29:56 +0700

    image ratio fixed bug fix 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,8 +26,10 @@
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                 @foreach ($featuredDecants as $decant)
                     <div class="border p-4 rounded-lg">
-                        <img src="{{ asset('storage/' . $decant->image) }}" alt="{{ $decant->name }}"
-                            class="w-full h-48 object-cover rounded" />
+                        <div class="relative w-full aspect-[3/4] bg-gray-100 rounded">
+                            <img src="{{ asset('storage/' . $decant->image) }}" alt="{{ $decant->name }}"
+                                class="absolute inset-0 w-full h-full object-contain p-2">
+                        </div>
                         <h3 class="text-xl font-semibold mt-4">{{ $decant->name }}</h3>
                         <p class="text-gray-600">{{ $decant->priceRange ?? 'Price not available' }}</p>
                     </div>
